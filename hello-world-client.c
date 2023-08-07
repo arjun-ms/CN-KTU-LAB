52fa63391d7d383fad205dc9335e12002190a629
Arjun M S <arjunachu123.aa@gmail.com>
2023-08-07 11:06:15 +0530

    commit

--- hello-world-client.c
+++ hello-world-client.c
@@ -13,16 +13,15 @@ int main(){
 	clientSocket = socket(AF_INET,SOCK_STREAM,0);
 	
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(8000);
+	serverAddr.sin_port = htons(9999);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
 	memset(serverAddr.sin_zero,'\0',sizeof(serverAddr.sin_zero));
 	
 	addrSize = sizeof(serverAddr);
-	connect(clientSocket,(struct sockaddr*)&serverAddr,addrSize);
 	
-	recv(clientSocket,buffer,1024,0);
-	printf("Data recieved : %s",buffer);
-	
+	connect(clientSocket,(struct sockaddr *)&serverAddr,addrSize);
 
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Data: %s",buffer);
 }
--- hello-world-server.c
+++ hello-world-server.c
@@ -8,27 +8,28 @@ int main(){
 	int welcomeSocket,newSocket;
 	char buffer[1024];
 	struct sockaddr_in serverAddr;
-	struct sockaddr_storage serverStorage;
 	socklen_t addrSize;
 	
 	welcomeSocket = socket(AF_INET,SOCK_STREAM,0);
 	
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(8000);
+	serverAddr.sin_port = htons(9999);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
 	memset(serverAddr.sin_zero,'\0',sizeof(serverAddr.sin_zero));
-	bind(welcomeSocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+	bind(welcomeSocket,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
 	
-	if(listen(welcomeSocket,5)==0)
-		printf("Listening\n");
-	else
-		printf("Error");
+	if(listen(welcomeSocket,5)==0){
+		printf("Listening..\n");
+	}
+	else{
+		printf("Error..\n");
+	}
 	
-	addrSize = sizeof(serverStorage);
-	newSocket = accept(welcomeSocket,(struct sockaddr*)&serverStorage,&addrSize);
-	strcpy(buffer,"Hello World\n");
-	send(newSocket,buffer,1024,0);
+	addrSize = sizeof(serverAddr);
 	
-
+	newSocket = accept(welcomeSocket,(struct sockaddr *)&serverAddr,&addrSize);
+	
+	strcpy(buffer,"hello");
+	send(newSocket,buffer,sizeof(buffer),0);
 }
